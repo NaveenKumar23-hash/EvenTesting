9f554676913e2763d1a6efa94a7ec2a9b1a07dcc
Naveenkumar23-hash <nk32294@gmail.com>
2022-01-18 16:39:29 +0530

    Done

---
+++ src/org/even/TestingEven.java
@@ -0,0 +1,18 @@
+package org.even;
+
+import java.util.Scanner;
+
+public class TestingEven {
+	public static void main(String[] args) {
+		int n;
+		Scanner s = new Scanner(System.in);
+	             n = s.nextInt();
+			   if (n%2==0) {
+				   System.out.println("Even Number");
+				
+			} else {
+	         System.out.println("Odd Number");
+			}
+					
+		}
+}
